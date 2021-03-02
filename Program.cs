083978f6e0e8659c2d4daaded996767f67efff35
da <david@gmail.com>
2021-03-02 21:14:34 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lowest_common_denominator
+{
+    class Program
+    {
+        static int lcd(int a, int b) {
+            if (a > b) {
+                int t = a;
+                a = b;
+                b = t;
+            }
+            if (b % a == 0)
+                return b;
+            int dif = b % a;
+            int dif2 = a % dif;
+            double mul = dif / dif2;
+            return (int)(((double)a/dif*mul)*b);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Lowest common denominator = " + lcd(a, b));
+            Console.ReadLine();
+        }
+    }
+}
