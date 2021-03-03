05e0ef633cc484ddd68013a800494aef9ada7835
Jenya Knyazev <davidknyazev409@gmail.com>
2021-03-03 11:01:29 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ namespace lowest_common_denominator
 {
     class Program
     {
+        static int gcd(int a,int b) {
+            if (b == 0)
+                return a;
+            return gcd(b, a % b);
+        }
         static int lcd(int a, int b) {
             if (a > b) {
                 int t = a;
@@ -20,7 +25,8 @@ namespace lowest_common_denominator
             int dif2 = a % dif;
             if (dif2 == 0)
                 return b*a/dif;
-            double mul =(double) dif/(int)((double)dif/dif2+(double)dif2/dif);
+            //double mul = (double)dif /(int)((double)dif/dif2+(double)dif2/dif);
+            double mul = (double)dif / gcd(dif, dif2);
             if (dif % dif2 == 0)
                 mul = dif / dif2;
             return (int)(((double)a/dif*mul)*b);
